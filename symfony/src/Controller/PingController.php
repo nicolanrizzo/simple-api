c863bc9a1ed725a57b11965835caee95f2152288
Nicola RIZZO <nicola.n.rizzo@gmail.com>
2018-03-23 09:29:42 +0100

    Smaller ping class code using JsonResponse

--- symfony/src/Controller/PingController.php
+++ symfony/src/Controller/PingController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PingController
@@ -12,11 +12,7 @@ class PingController
      */
     public function pingAction()
     {
-        $response = new Response();
-        $response->setContent(json_encode(array(
-            'ping' => 'pong',
-        )));
-        $response->headers->set('Content-Type', 'application/json');
+        $response = new JsonResponse(array('ping' => 'pong'));
 
         return $response;
     }
